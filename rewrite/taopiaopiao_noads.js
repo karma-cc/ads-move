2d85962aedd2a495cb6b2e8695f0bcfc64c5dd24
karma-cc <karmaccbot@gmail.com>
2026-04-30 17:58:15 +0800

    Create taopiaopiao_noads.js

---
+++ rewrite/taopiaopiao_noads.js
@@ -0,0 +1,3 @@
+[rewrite_local]
+
+^https:\/\/acs\.m\.taobao\.com\/gw\/mtop\.film\.mtopadvertiseapi\.queryloadingbanner\/1\.0 url script-response-body https://raw.githubusercontent.com/karma-cc/ads-move/refs/heads/main/JS/taopiaopiao_noads.js
