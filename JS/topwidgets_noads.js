59c3199db8cba01b57fec30fb03c950c9ac6cd44
karma-cc <karmaccbot@gmail.com>
2026-04-30 23:36:51 +0800

    Update topwidgets_noads.js

--- JS/topwidgets_noads.js
+++ JS/topwidgets_noads.js
@@ -1,58 +1,89 @@
 /*
 Top Widgets+ - 首页 VIP / 广告横幅清理
-目标：
+
+处理接口：
 1. /bff/advertisement/query
 2. /app/resource/vip/list
+3. /app/home
 */
 
-let url = $request.url;
+let url = $request.url || "";
 let body = $response.body || "";
 
-function emptySuccess(extraData) {
+const killWords = /VIP|vip|会员|开通|专属功能|广告|advertisement|ad|banner|开通VIP|获取更多专属功能/i;
+
+function emptySuccess(data) {
   return {
     ret: 0,
     msg: "success",
-    data: extraData || {}
+    data: data || {}
   };
+}
+
+function cleanArray(arr) {
+  if (!Array.isArray(arr)) return arr;
+
+  return arr.filter(item => {
+    try {
+      return !killWords.test(JSON.stringify(item));
+    } catch (e) {
+      return true;
+    }
+  });
+}
+
+function deepClean(obj) {
+  if (Array.isArray(obj)) {
+    return obj
+      .map(item => deepClean(item))
+      .filter(item => {
+        try {
+          return !killWords.test(JSON.stringify(item));
+        } catch (e) {
+          return true;
+        }
+      });
+  }
+
+  if (obj && typeof obj === "object") {
+    for (let key of Object.keys(obj)) {
+      let value = obj[key];
+
+      // 命中明显广告 / VIP 字段名，直接清空
+      if (killWords.test(key)) {
+        obj[key] = Array.isArray(value) ? [] : null;
+        continue;
+      }
+
+      // 字符串内容命中，清空
+      if (typeof value === "string" && killWords.test(value)) {
+        obj[key] = "";
+        continue;
+      }
+
+      // 递归处理对象和数组
+      if (value && typeof value === "object") {
+        obj[key] = deepClean(value);
+      }
+    }
+  }
+
+  return obj;
 }
 
 try {
   let obj = JSON.parse(body);
 
-  // 首页广告 / 推广查询
+  // 1. 首页广告 / 推广查询
   if (/\/bff\/advertisement\/query/.test(url)) {
     obj.ret = 0;
     obj.msg = "success";
     obj.data = {};
 
-    // 常见广告字段兜底清空
-    const keys = [
-      "list",
-      "advertisements",
-      "advertisement",
-      "ads",
-      "adList",
-      "banner",
-      "banners",
-      "popup",
-      "popups",
-      "vipBanner",
-      "homeBanner",
-      "homeVipBanner",
-      "floatWindow",
-      "floatView"
-    ];
-
-    for (const k of keys) {
-      if (Object.prototype.hasOwnProperty.call(obj.data, k)) {
-        obj.data[k] = Array.isArray(obj.data[k]) ? [] : null;
-      }
-    }
-
     body = JSON.stringify(obj);
   }
 
-  // VIP 资源推广列表
+  // 2. VIP 资源推广列表
   else if (/\/app\/resource\/vip\/list/.test(url)) {
     obj.ret = 0;
     obj.msg = "success";
@@ -64,8 +95,27 @@ try {
 
     body = JSON.stringify(obj);
   }
+
+  // 3. 首页配置，清理“开通 VIP 获取更多专属功能”横幅
+  else if (/\/app\/home\?/.test(url)) {
+    obj.ret = 0;
+    obj.msg = "success";
+    obj.data = obj.data || {};
+
+    // 首页常见数组字段
+    obj.data.categories = cleanArray(obj.data.categories);
+    obj.data.groups = cleanArray(obj.data.groups);
+    obj.data.resources = cleanArray(obj.data.resources);
+    obj.data.grids = cleanArray(obj.data.grids);
+    obj.data.tags = cleanArray(obj.data.tags);
+
+    // 兜底：递归清理所有包含 VIP / 会员 / 开通 / 专属功能 的内容
+    obj.data = deepClean(obj.data);
+
+    body = JSON.stringify(obj);
+  }
 } catch (e) {
-  // 如果遇到空 body / 压缩未解开 / 非 JSON，返回一个空成功结构
+  // 如果响应体为空、压缩未解开、或不是 JSON，返回空成功结构
   if (/\/bff\/advertisement\/query/.test(url)) {
     body = JSON.stringify(emptySuccess({}));
   } else if (/\/app\/resource\/vip\/list/.test(url)) {
@@ -74,6 +124,21 @@ try {
       resources: [],
       vipList: [],
       timestamp: Date.now()
+    }));
+  } else if (/\/app\/home\?/.test(url)) {
+    body = JSON.stringify(emptySuccess({
+      categories: [],
+      groups: [],
+      resources: [],
+      grids: [],
+      tags: [],
+      removals: {
+        categories: [],
+        groups: [],
+        grids: [],
+        resources: []
+      },
+      lastSequenceId: 0
     }));
   }
 }
