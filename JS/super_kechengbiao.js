0b73db647796b4e1e71716ffa2cc3b1253f4b3ec
karma-cc <karmaccbot@gmail.com>
2026-04-30 17:16:07 +0800

    Update super_kechengbiao.js

--- JS/super_kechengbiao.js
+++ JS/super_kechengbiao.js
@@ -1,9 +1,4 @@
-/*
-Friday - 开屏广告清理
-根据本次抓包：
-1. /V2/splash/getSplashV2.action 返回 adSplashId = 83
-2. /d/json/e/1.1?pos=83 返回 “倍孜5.0-开机-iOS备用” 广告列表
-*/
+
 
 let url = $request.url;
 let body = $response.body || "";
@@ -11,7 +6,7 @@ let body = $response.body || "";
 try {
   let obj = JSON.parse(body);
 
-  // 1. App 自有开屏配置接口
+
   if (/\/V2\/splash\/getSplashV2\.action/.test(url)) {
     obj.status = 0;
     obj.data = {
@@ -22,9 +17,9 @@ try {
     };
   }
 
-  // 2. 第三方广告列表接口
+
   else if (/\/d\/json\/e\/1\.1/.test(url)) {
-    // 只处理开屏广告位 pos=83，避免误伤其他广告位
+
     if (/[\?&]?pos=83(?:&|$)/.test($request.body || "") || /pos=83/.test($request.body || "")) {
       obj.ads = [];
     } else if (Array.isArray(obj.ads)) {
